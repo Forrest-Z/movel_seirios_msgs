a53c01f3bbea1280966324f5be3310d3af4fc754
Ryan Lee <ryan@movel.ai>
2020-12-28 16:57:01 +0800

    wip

---
+++ src/robot_center/include/robot_center/control_center.hpp
@@ -0,0 +1,15 @@
+#ifndef ROBOTCENTER_H
+#define ROBOTCENTER_H
+
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+#include <cxxopts.hpp>
+#include <string>
+#include <iostream>
+
+class RobotCenter{
+
+}
+
+
+#endif
