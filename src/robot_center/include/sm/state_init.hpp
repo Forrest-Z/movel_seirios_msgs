3a88941efd307f832534d9ce0c088cacaaeccba5
Ryan Lee <ryan@movel.ai>
2020-12-29 14:57:28 +0800

    wip

--- src/robot_center/include/sm/state_init.hpp
+++ src/robot_center/include/sm/state_init.hpp
@@ -1,57 +1,92 @@
 #ifndef ROBOT_STATE_INIT_H
 #define ROBOT_STATE_INIT_H
-#include <g3log/g3log.hpp>
+#include <movel_g3log_vendor/include/g3log/g3log.hpp>
 #include <uv.h>
 //#include <helpers/rabbit_helper.hpp>
 //#include <helpers/mongo_helper.hpp>
 //#include <helpers/ros_helper.hpp>
 
-struct EvToInit {};
-
+struct EvToInit
+{
+};
 
 // Guards
-struct GuardEntryInit//guardEntryInit;
+struct GuardEntryInit  // guardEntryInit;
 {
-  bool operator()() const { LOG(INFO) << "guard exit init"; return true; }
+  bool operator()() const
+  {
+    LOG(INFO) << "guard exit init";
+    return true;
+  }
 };
-struct GuardToInit// guardToInit;
+struct GuardToInit  // guardToInit;
 {
-  bool operator()() const { LOG(INFO) << "guard exit init"; return true; }
+  bool operator()() const
+  {
+    LOG(INFO) << "guard exit init";
+    return true;
+  }
 };
 struct GuardExitInit
 {
-  bool operator()() const { LOG(INFO) << "guard exit init"; return true; }
+  bool operator()() const
+  {
+    LOG(INFO) << "guard exit init";
+    return true;
+  }
 };
 struct GuardUnexpectedInit
 {
-  bool operator()() const { LOG(INFO) << "guard unexpected init"; return true; }
+  bool operator()() const
+  {
+    LOG(INFO) << "guard unexpected init";
+    return true;
+  }
 };
 struct GuardExceptionInit
 {
-  bool operator()() const { LOG(INFO) << "guard unexpected init"; return true; }
+  bool operator()() const
+  {
+    LOG(INFO) << "guard unexpected init";
+    return true;
+  }
 };
-
 
 // Actions
-struct ActEntryInit// {} actEntryInit;
+struct ActEntryInit  // {} actEntryInit;
 {
-  void operator()() { LOG(INFO) << "action exit init"; }
+  void operator()()
+  {
+    LOG(INFO) << "action exit init";
+  }
 };
-struct ActToInit // {} actToInit;
+struct ActToInit  // {} actToInit;
 {
-  void operator()() { LOG(INFO) << "action exit init"; }
+  void operator()()
+  {
+    LOG(INFO) << "action exit init";
+  }
 };
 struct ActExitInit
 {
-  void operator()() { LOG(INFO) << "action exit init"; }
+  void operator()()
+  {
+    LOG(INFO) << "action exit init";
+  }
 };
 struct ActUnexpectedInit
 {
-  void operator()() { LOG(INFO) << "action unexpected init"; }
+  void operator()()
+  {
+    LOG(INFO) << "action unexpected init";
+  }
 };
 struct ActExceptionInit
 {
-  void operator()() { LOG(INFO) << "action unexpected init"; }
+  void operator()()
+  {
+    LOG(INFO) << "action unexpected init";
+  }
 };
 
 #endif
