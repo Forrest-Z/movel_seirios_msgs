48c79d25dfd54b9cfdd23bc85baaa975cc5a222b
Ryan Lee <ryan@movel.ai>
2020-12-31 09:52:51 +0800

    removed robot_center cause it was breaking pkg build, but added post inst script to debian packaging

--- src/robot_center/src/helpers/mongo_helper.cpp
+++
@@ -1,6 +0,0 @@
-#include <helpers/mongo_helper.hpp>
-
-
-mongocxx::instance instance{}; // This should be done only once.
-mongocxx::uri uri("mongodb://localhost:27017");
-mongocxx::client client(uri);
