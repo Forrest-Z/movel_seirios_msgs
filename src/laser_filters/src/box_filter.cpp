10edd914519fa7f6b2fc90b0c1e206c22c00d10a
iris <iris@movel.ai>
2021-02-26 09:18:18 +0800

    feature ready

---
+++ src/laser_filters/src/box_filter.cpp
@@ -0,0 +1,328 @@
+/*
+ *  Software License Agreement (BSD License)
+ *
+ *  Robot Operating System code by the University of Osnabrück
+ *  Copyright (c) 2015, University of Osnabrück
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   1. Redistributions of source code must retain the above 
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer.
+ *
+ *   2. Redistributions in binary form must reproduce the above 
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *   3. Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ *  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *
+ *  box_filter.cpp
+ *
+ *  author: Sebastian Pütz <spuetz@uni-osnabrueck.de>
+ */
+
+#include "laser_filters/box_filter.h"
+#include <ros/ros.h>
+
+laser_filters::LaserScanBoxFilter::LaserScanBoxFilter(){}
+
+bool laser_filters::LaserScanBoxFilter::configure(){
+  up_and_running_ = true;
+  double min_x = 0, min_y = 0, min_z = 0, max_x = 0, max_y = 0, max_z = 0;
+  // switch_ = true; //[1.0.2] filter switch
+
+  //[1.0.2]
+  ros::NodeHandle private_nh("~" + getName()); //create private namespace
+  dyn_server_.reset(new dynamic_reconfigure::Server<laser_filters::BoxFilterConfig>(own_mutex_, private_nh)); 
+  dynamic_reconfigure::Server<laser_filters::BoxFilterConfig>::CallbackType f; 
+  f = boost::bind(&laser_filters::LaserScanBoxFilter::reconfigureCB, this, _1, _2 ); 
+  dyn_server_ -> setCallback(f); 
+
+  bool box_frame_set = getParam("box_frame", box_frame_); 
+  bool x_max_set = getParam("max_x", max_x);
+  bool y_max_set = getParam("max_y", max_y);
+  bool z_max_set = getParam("max_z", max_z);
+  bool x_min_set = getParam("min_x", min_x);
+  bool y_min_set = getParam("min_y", min_y);
+  bool z_min_set = getParam("min_z", min_z);
+  bool invert_set = getParam("invert", invert_filter);
+  
+  ROS_INFO("Box filter(%s) started", getName().c_str());
+
+  max_.setX(max_x); max_.setY(max_y); max_.setZ(max_z);
+  min_.setX(min_x); min_.setY(min_y); min_.setZ(min_z);
+  
+  if(!box_frame_set){
+    ROS_ERROR("box_frame is not set!");
+  }
+  if(!x_max_set){
+    ROS_ERROR("max_x is not set!");
+  }
+  if(!y_max_set){
+    ROS_ERROR("max_y is not set!");
+  }
+  if(!z_max_set){
+    ROS_ERROR("max_z is not set!");
+  }
+  if(!x_min_set){
+    ROS_ERROR("min_x is not set!");
+  }
+  if(!y_min_set){
+    ROS_ERROR("min_y is not set!");
+  }
+  if(!z_min_set){
+    ROS_ERROR("min_z is not set!");
+  }
+  if(!invert_set){
+    ROS_INFO("invert filter not set, assuming false");
+    invert_filter=false;
+  }
+
+  //[1.0.2] Generate 3D polygon for polygonstamped msg
+  polygon_ = make3DPolygon();
+  polygon_frame_ = box_frame_;
+
+  //[1.0.2] update dynamic config parameters
+  param_config.min_x = min_x;
+  param_config.min_y = min_y;
+  param_config.min_z = min_z;
+  param_config.max_x = max_x;
+  param_config.max_y = max_y;
+  param_config.max_z = max_z;
+  param_config.invert = invert_filter;
+  if(getParam("switch_",switch_)) { //[1.0.2] Added switch to turn filter on/off
+    param_config.switch_ = switch_;
+  }
+  dyn_server_->updateConfig(param_config);
+
+  //[1.0.2] Added polygon publisher
+  polygon_pub_ = private_nh.advertise<geometry_msgs::PolygonStamped>("box_polygon", 1);
+
+  return box_frame_set && x_max_set && y_max_set && z_max_set &&
+    x_min_set && y_min_set && z_min_set;
+}
+
+//[1.0.2] Generate 3D point
+geometry_msgs::Point32 laser_filters::LaserScanBoxFilter::Point3D(float &&x, float &&y, float &&z) 
+{
+  geometry_msgs::Point32 point;
+  point.x = x; point.y = y; point.z = z; 
+  return point;
+}
+
+//[1.0.2] Generate 2D polygon message
+geometry_msgs::Polygon laser_filters::LaserScanBoxFilter::make2DPolygon() 
+{
+  geometry_msgs::Polygon polygon;
+  geometry_msgs::Point32 point;
+  //Top surface of box
+  point = Point3D( max_.x() , max_.y() , max_.z() ); //Point A 
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , max_.y() , max_.z() ); //Point B
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , min_.y() , max_.z() ); //C
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , min_.y() , max_.z() ); //D
+  polygon.points.push_back(point);
+  return polygon;
+}
+
+//[1.0.2] Generate 3D wiremesh of box
+geometry_msgs::Polygon laser_filters::LaserScanBoxFilter::make3DPolygon() 
+{
+  geometry_msgs::Polygon polygon;
+  geometry_msgs::Point32 point;
+
+  //Top surface of box
+  point = Point3D( max_.x() , max_.y() , max_.z() ); //Point A 
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , max_.y() , max_.z() ); //Point B
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , min_.y() , max_.z() ); //C
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , min_.y() , max_.z() ); //D
+  polygon.points.push_back(point);
+  //Side of box
+  point = Point3D( max_.x() , min_.y() , min_.z() ); //E
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , max_.y() , min_.z() ); //F
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , max_.y() , max_.z() ); //A 
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , max_.y() , min_.z() ); //F
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , max_.y() , min_.z() ); //G
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , max_.y() , max_.z() ); //B
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , max_.y() , min_.z() ); //G
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , min_.y() , min_.z() ); //H
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , min_.y() , max_.z() ); //C
+  polygon.points.push_back(point);
+  point = Point3D( min_.x() , min_.y() , min_.z() ); //H
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , min_.y() , min_.z() ); //E
+  polygon.points.push_back(point);
+  point = Point3D( max_.x() , min_.y() , max_.z() ); //D
+  polygon.points.push_back(point);
+
+  return polygon;
+}
+
+//[1.0.2] Added Dynamic Reconfigure callback
+void laser_filters::LaserScanBoxFilter::reconfigureCB(laser_filters::BoxFilterConfig &config, uint32_t level) 
+{ 
+  min_.setX(config.min_x);
+  min_.setY(config.min_y);
+  min_.setZ(config.min_z);
+  max_.setX(config.max_x);
+  max_.setY(config.max_y);
+  max_.setZ(config.max_z);
+  invert_filter = param_config.invert;
+  switch_ = config.switch_;
+  polygon_ = make3DPolygon();
+}
+
+bool laser_filters::LaserScanBoxFilter::update( const sensor_msgs::LaserScan& input_scan, sensor_msgs::LaserScan &output_scan)
+{
+  //[1.0.2]
+  boost::recursive_mutex::scoped_lock lock(own_mutex_);
+
+  //[1.0.2] Generate PolygonStamped msg and publish it
+  geometry_msgs::PolygonStamped polygon_stamped;
+  polygon_stamped.header.frame_id = polygon_frame_;
+  polygon_stamped.header.stamp = ros::Time::now();
+  polygon_stamped.polygon = polygon_;
+  polygon_pub_.publish(polygon_stamped);
+
+  //[1.0.2] switch on/off filter
+  if (!switch_)
+  {
+    output_scan = input_scan;
+    up_and_running_ = true;
+    return true;
+  }
+
+  output_scan = input_scan;
+  sensor_msgs::PointCloud2 laser_cloud;
+  
+  std::string error_msg;
+
+
+
+  bool success = tf_.waitForTransform(
+    box_frame_,
+    input_scan.header.frame_id,
+    input_scan.header.stamp + ros::Duration().fromSec(input_scan.ranges.size()*input_scan.time_increment),
+    ros::Duration(1.0),
+    ros::Duration(0.01),
+    &error_msg
+  );
+  if(!success){
+    ROS_WARN("Could not get transform, ignoring laser scan! %s", error_msg.c_str());
+    return false;
+  }
+
+  try{
+    projector_.transformLaserScanToPointCloud(box_frame_, input_scan, laser_cloud, tf_);
+  }
+  catch(tf::TransformException& ex){
+    if(up_and_running_){
+      ROS_WARN_THROTTLE(1, "Dropping Scan: Tansform unavailable %s", ex.what());
+      return true;
+    }
+    else
+    {
+      ROS_INFO_THROTTLE(.3, "Ignoring Scan: Waiting for TF");
+    }
+    return false;
+  }
+  const int i_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "index");
+  const int x_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "x");
+  const int y_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "y");
+  const int z_idx_c = sensor_msgs::getPointCloud2FieldIndex(laser_cloud, "z");
+
+  if(i_idx_c == -1 || x_idx_c == -1 || y_idx_c == -1 || z_idx_c == -1){
+      ROS_INFO_THROTTLE(.3, "x, y, z and index fields are required, skipping scan");
+
+  }
+
+  const int i_idx_offset = laser_cloud.fields[i_idx_c].offset;
+  const int x_idx_offset = laser_cloud.fields[x_idx_c].offset;
+  const int y_idx_offset = laser_cloud.fields[y_idx_c].offset;
+  const int z_idx_offset = laser_cloud.fields[z_idx_c].offset;
+
+  const int pstep = laser_cloud.point_step;
+  const long int pcount = laser_cloud.width * laser_cloud.height;
+  const long int limit = pstep * pcount;
+
+  int i_idx, x_idx, y_idx, z_idx;  
+  for(
+    i_idx = i_idx_offset,
+    x_idx = x_idx_offset,
+    y_idx = y_idx_offset,
+    z_idx = z_idx_offset;
+
+    x_idx < limit;
+
+    i_idx += pstep,
+    x_idx += pstep,
+    y_idx += pstep,
+    z_idx += pstep)
+  {
+
+    // TODO works only for float data types and with an index field
+    // I'm working on it, see https://github.com/ros/common_msgs/pull/78 
+    float x = *((float*)(&laser_cloud.data[x_idx]));
+    float y = *((float*)(&laser_cloud.data[y_idx]));
+    float z = *((float*)(&laser_cloud.data[z_idx]));
+    int index = *((int*)(&laser_cloud.data[i_idx]));
+
+    tf::Point point(x, y, z);
+
+    if(!invert_filter){
+      if(inBox(point)){
+        output_scan.ranges[index] = std::numeric_limits<float>::quiet_NaN();
+      }
+    }
+    else{
+      if(!inBox(point)){
+        output_scan.ranges[index] = std::numeric_limits<float>::quiet_NaN();
+      }
+    }
+
+  }
+  up_and_running_ = true;
+  return true;
+}
+
+bool laser_filters::LaserScanBoxFilter::inBox(tf::Point &point){
+  return
+    point.x() < max_.x() && point.x() > min_.x() && 
+    point.y() < max_.y() && point.y() > min_.y() &&
+    point.z() < max_.z() && point.z() > min_.z();
+}
+
---
+++ src/laser_filters/src/dynamic_scan_to_scan_filter_chain.cpp
@@ -0,0 +1,120 @@
+//took out the copyright notice
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "message_filters/subscriber.h"
+#include "tf/message_filter.h"
+#include "tf/transform_listener.h"
+#include "filters/filter_chain.h"
+#include <movel_hasp_vendor/license.h>
+
+class ScanToScanFilterChain
+{
+protected:
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+
+  // Components for tf::MessageFilter
+  tf::TransformListener *tf_;
+  message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
+  tf::MessageFilter<sensor_msgs::LaserScan> *tf_filter_;
+  double tf_filter_tolerance_;
+
+  filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
+
+  // Components for publishing
+  sensor_msgs::LaserScan msg_;
+  ros::Publisher output_pub_;
+
+  // Deprecation helpers
+  ros::Timer deprecation_timer_;
+  bool using_filter_chain_deprecated_;
+
+
+public:
+  // Constructor
+  ScanToScanFilterChain() : private_nh_("~"), scan_sub_(nh_, "scan_front", 50), tf_(NULL), tf_filter_(NULL), filter_chain_("sensor_msgs::LaserScan")
+  {
+    // Configure filter chain
+    using_filter_chain_deprecated_ = private_nh_.hasParam("init/filter_chain");
+
+    if (using_filter_chain_deprecated_)
+      filter_chain_.configure("init/filter_chain", private_nh_);
+    else
+      filter_chain_.configure("init/scan_filter_chain", private_nh_);
+    
+    std::string tf_message_filter_target_frame;
+
+    if (private_nh_.hasParam("init/tf_message_filter_target_frame"))
+    {
+      private_nh_.getParam("init/tf_message_filter_target_frame", tf_message_filter_target_frame);
+
+      private_nh_.param("init/tf_message_filter_tolerance", tf_filter_tolerance_, 0.03);
+
+      tf_ = new tf::TransformListener();
+      tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, *tf_, "", 50);
+      tf_filter_->setTargetFrame(tf_message_filter_target_frame);
+      tf_filter_->setTolerance(ros::Duration(tf_filter_tolerance_));
+
+      // Setup tf::MessageFilter generates callback
+      tf_filter_->registerCallback(boost::bind(&ScanToScanFilterChain::callback, this, _1));
+    }
+    else 
+    {
+      // Pass through if no tf_message_filter_target_frame
+      scan_sub_.registerCallback(boost::bind(&ScanToScanFilterChain::callback, this, _1));
+    }
+    
+    output_pub_ = nh_.advertise<sensor_msgs::LaserScan>("scan_filtered", 1000); // Advertise output
+    
+    deprecation_timer_ = nh_.createTimer(ros::Duration(5.0), boost::bind(&ScanToScanFilterChain::deprecation_warn, this, _1)); // Set up deprecation printoutb
+  }
+
+  // Destructor
+  ~ScanToScanFilterChain()
+  {
+    if (tf_filter_)
+      delete tf_filter_;
+    if (tf_)
+      delete tf_;
+  }
+  
+  // Deprecation warning callback
+  void deprecation_warn(const ros::TimerEvent& e)
+  {
+    if (using_filter_chain_deprecated_)
+      ROS_WARN("Use of '~filter_chain' parameter in scan_to_scan_filter_chain has been deprecated. Please replace with '~scan_filter_chain'.");
+  }
+
+  void callback(const sensor_msgs::LaserScan::ConstPtr& msg_in)
+  {
+    if (filter_chain_.update(*msg_in, msg_))
+    {
+      output_pub_.publish(msg_); //only publish result if filter succeeded
+    } 
+    else 
+    {
+      ROS_ERROR_THROTTLE(1, "Filtering the scan from time %i.%i failed.", msg_in->header.stamp.sec, msg_in->header.stamp.nsec);
+    }
+  }
+};
+
+int main(int argc, char **argv)
+{
+  #ifdef MOVEL_LICENSE                                                                                                    
+  MovelLicense ml(23);                                                                                                   
+  if (!ml.login())                                                                                                      
+    return 1;                                                                                                           
+  #endif
+
+  ros::init(argc, argv, "dynamic_scan_to_scan_filter_chain");
+
+  ScanToScanFilterChain t;
+
+  ros::spin();
+  #ifdef MOVEL_LICENSE                                                                                                    
+  ml.logout();          
+  #endif    
+
+  return 0;
+}
