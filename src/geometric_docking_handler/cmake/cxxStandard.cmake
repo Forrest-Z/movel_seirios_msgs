89263e20409a181c057fb39a6169904ca9124b40
Ryan Arya Pratama <ryan.arya@movel.ai>
2021-03-01 02:33:16 +0000

    Feature/geometric docking

---
+++ src/geometric_docking_handler/cmake/cxxStandard.cmake
@@ -0,0 +1,36 @@
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" AND "${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS 6.0.0)
+  message(FATAL_ERROR "${PROJECT_NAME} requires GCC >= 6.0.0")
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND "${CMAKE_CXX_COMPILER_VERSION}" VERSION_LESS 3.5.0)
+  message(FATAL_ERROR "${PROJECT_NAME} requires Clang >= 3.5.0")
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 19.0)
+  message(FATAL_ERROR "${PROJECT_NAME} requires Visual Studio 14 2015 at least")
+endif()
+
+include(CheckCXXCompilerFlag)
+check_cxx_compiler_flag(-std=c++14 HAS_CXX14_FLAG)
+check_cxx_compiler_flag(-std=c++17 HAS_CXX17_FLAG)
+check_cxx_compiler_flag(-std=c++2a HAS_CXX20_FLAG)
+
+if(HAS_CXX20_FLAG)
+  set(CMAKE_CXX_STANDARD 20)
+elseif(HAS_CXX17_FLAG)
+  set(CMAKE_CXX_STANDARD 17)
+elseif(HAS_CXX14_FLAG)
+  set(CMAKE_CXX_STANDARD 14)
+else()
+    message(FATAL_ERROR "${PROJECT_NAME} requires c++14")
+endif()
+
+if(NOT (DEFINED CMAKE_CXX_STANDARD) OR CMAKE_CXX_STANDARD STREQUAL "" OR CMAKE_CXX_STANDARD LESS 14)
+    message(FATAL_ERROR "${PROJECT_NAME} requires c++14")
+endif()
+
+set(IS_COMPILER_GCC_LIKE FALSE)
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR
+    "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang" OR
+    "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" )
+
+  set(IS_COMPILER_GCC_LIKE TRUE)
+endif()
+set(CXX_STANDARD_REQUIRED ON)
+message("${BoldCyan}CMAKE_CXX_STANDARD set to ${CMAKE_CXX_STANDARD}\n\n${ColourReset}")
