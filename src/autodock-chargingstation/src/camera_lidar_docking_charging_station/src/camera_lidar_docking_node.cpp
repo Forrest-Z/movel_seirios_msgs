c8725a968f293bdf94c2a191dbe35fbf4b5cf86e
Robertsen Putra Sugianto <robertsen@movel.ai>
2021-03-02 12:13:02 +0700

    cleaned up the package

--- src/autodock-chargingstation/src/camera_lidar_docking_charging_station/src/camera_lidar_docking_node.cpp
+++ src/autodock-chargingstation/src/camera_lidar_docking_charging_station/src/camera_lidar_docking_node.cpp
@@ -2,13 +2,20 @@
 #include <ros/ros.h>
 #include <camera_docking_/camera_docking.h>
 //#include "lidar_docking_/LidarDocking.hpp"
-#include<cameralidarDock/camera_lidar_docking.h>
+#include <cameralidarDock/camera_lidar_docking.h>
+#include <movel_hasp_vendor/license.h>
 
 using namespace std;
 using namespace cv;
 
 
 int main(int argc, char ** argv) {
+    #ifdef MOVEL_LICENSE                                                                                                    
+        MovelLicense ml(18);                                                                                                   
+        if (!ml.login())                                                                                                      
+            return 1;                                                                                                           
+    #endif
+
     ros::init(argc, argv, "camera_lidar_docking");
     ros::NodeHandle nodeHandle("~");
 
@@ -16,6 +23,9 @@ int main(int argc, char ** argv) {
     CameraLidar camera_lidar(nodeHandle);
     camera_lidar.startAutoDock = nodeHandle.advertiseService("StartAutoDocking", &CameraLidar::autoDock, &camera_lidar);
     ros::spin();
+    #ifdef MOVEL_LICENSE                                                                                                    
+        ml.logout();          
+    #endif     
 
     return 0;
 }
