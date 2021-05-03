68ff97b6481117b38914b38bd4b658dd1dfdba1c
ongkoonhan <koonhan@movel.ai>
2021-05-03 07:48:42 +0000

    added best_effort (v1) for nav_handler;

--- src/task_supervisor/include/task_supervisor/plugins/navigation_handler.h
+++ src/task_supervisor/include/task_supervisor/plugins/navigation_handler.h
@@ -11,7 +11,12 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <std_msgs/Float64.h>
 #include <std_srvs/SetBool.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/GetPlan.h>
+
 #include <boost/thread/mutex.hpp>
+
 
 namespace task_supervisor
 {
@@ -26,46 +31,70 @@ private:
   std::string p_human_detection_topic_;
   std::string p_enable_human_detection_msg_;
   std::string p_disable_human_detection_msg_;
-
+  bool p_enable_best_effort_goal_;
+  bool p_normal_nav_if_best_effort_unavailable_;
+  // variables
   std::shared_ptr<actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>> nav_ac_ptr_;
-
-  bool loadParams();
-  bool start_ActionClient();
-
   boost::mutex mtx_;
   bool enable_human_detection_;
   double human_detection_score_;
   bool task_cancelled_;
+  geometry_msgs::Pose robot_pose_;
+//   bool wait_for_robot_pose_;
+  // topics/services
   ros::ServiceServer enable_human_detection_srv_;
+  ros::ServiceServer enable_best_effort_goal_srv_;
+  ros::ServiceClient make_plan_client_;
+  ros::ServiceClient make_reachable_plan_client_;   // planner_utils
   ros::Subscriber human_detection_sub_;
+  ros::Subscriber robot_pose_sub_;
 
+  bool loadParams();
+  bool start_ActionClient();
   /**
      * @brief Method called when navigation task is received while human detection is enabled
      * @param goal Navigation goal received
      */
   void startWithDetection(move_base_msgs::MoveBaseGoal goal);
-
   /**
      * @brief Callback when navigation_handler/enable_human_detection service is called
      */
   bool enableHumanDetectionCB(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
-
   /**
      * @brief Callback on human detection score topic published by human_detection node
      */
   void humanDetectionCB(const std_msgs::Float64::ConstPtr& msg);
-
+  /**
+     * @brief Callback when navigation_handler/enable_best_effort_goal service is called
+     */
+  bool enableBestEffortGoalCB(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+  /**
+     * @brief Callback on robot pose topic /pose
+     */
+  void robotPoseCB(const geometry_msgs::Pose::ConstPtr& msg);
   /**
      * @brief Loop during navigation to check for pause/resume
      */
-  void navigationLoop(move_base_msgs::MoveBaseGoal goal);
+  void navigationLoop(const move_base_msgs::MoveBaseGoal& goal);
+  /**
+     * @brief 
+     */
+  void navigationAttemptGoal(const move_base_msgs::MoveBaseGoal& goal);
+  /**
+     * @brief 
+     */
+  void navigationDirect(const geometry_msgs::Pose& goal);
+  /**
+     * @brief 
+     */
+  void navigationBestEffort(const geometry_msgs::Pose& goal);
+
 public:
-  NavigationHandler();
+   
+   NavigationHandler();
   ~NavigationHandler(){};
 
-  bool setupHandler();
-
-  /**
+   /**
      * @brief Method called by task_supervisor when a navigation task is received
      * @param task Relevant task passed to handler by task supervisor
      * @param error_message Error message returned by this handler if execution fails
@@ -73,7 +102,9 @@ public:
      */
   virtual ReturnCode runTask(movel_seirios_msgs::Task& task, std::string& error_message);
 
+  bool setupHandler();
   void cancelTask();
+  
 };
 }  // namespace task_supervisor
 
--- src/task_supervisor/src/plugins/navigation_handler.cpp
+++ src/task_supervisor/src/plugins/navigation_handler.cpp
@@ -2,6 +2,7 @@
 #include <task_supervisor/json.hpp>
 #include <pluginlib/class_list_macros.h>
 
+
 PLUGINLIB_EXPORT_CLASS(task_supervisor::NavigationHandler, task_supervisor::TaskHandler);
 
 using json = nlohmann::json;
@@ -23,8 +24,12 @@ bool NavigationHandler::setupHandler()
     ROS_FATAL("[%s] Error during parameter loading. Shutting down.", name_.c_str());
     return false;
   }
-  enable_human_detection_srv_ = nh_handler_.advertiseService("enable_human_detection", &NavigationHandler::enableHumanDetectionCB, this);
+  enable_human_detection_srv_ = nh_handler_.advertiseService("/enable_human_detection", &NavigationHandler::enableHumanDetectionCB, this);
+  enable_best_effort_goal_srv_ = nh_handler_.advertiseService("/enable_best_effort_goal", &NavigationHandler::enableBestEffortGoalCB, this);
+  make_plan_client_ = nh_handler_.serviceClient<nav_msgs::GetPlan>("/move_base/make_plan");
+  make_reachable_plan_client_ = nh_handler_.serviceClient<nav_msgs::GetPlan>("/make_reachable_plan");
   human_detection_sub_ = nh_handler_.subscribe(p_human_detection_topic_, 1, &NavigationHandler::humanDetectionCB, this);
+  robot_pose_sub_ = nh_handler_.subscribe("/pose", 1, &NavigationHandler::robotPoseCB, this);
   return true;
 }
 
@@ -36,7 +41,7 @@ bool NavigationHandler::loadParams()
            name_.c_str());
   
   // TODO: clean up param loading using lambda function
-  
+
   // server_timeout
   if (!nh_handler_.getParam("server_timeout", p_server_timeout_)) {
     ROS_ERROR("[%s] Failed to load parameter: server_timeout", name_.c_str());
@@ -79,7 +84,19 @@ bool NavigationHandler::loadParams()
     return false;
   }
   ROS_INFO("[%s] disable_human_detection_msg: %s", name_.c_str(), p_disable_human_detection_msg_.c_str());
-
+  // enable_best_effort_goal
+  if (!nh_handler_.getParam("enable_best_effort_goal", p_enable_best_effort_goal_)) {
+    ROS_ERROR("[%s] Failed to load parameter: enable_best_effort_goal", name_.c_str());
+    return false;
+  }
+  ROS_INFO("[%s] enable_best_effort_goal: %s", name_.c_str(), p_enable_best_effort_goal_ ? "true" : "false");
+  // normal_nav_if_best_effort_unavailable
+  if (!nh_handler_.getParam("normal_nav_if_best_effort_unavailable", p_normal_nav_if_best_effort_unavailable_)) {
+    ROS_ERROR("[%s] Failed to load parameter: normal_nav_if_best_effort_unavailable", name_.c_str());
+    return false;
+  }
+  ROS_INFO("[%s] normal_nav_if_best_effort_unavailable: %s", name_.c_str(), p_normal_nav_if_best_effort_unavailable_ ? "true" : "false");
+  
   return true;
 }
 
@@ -102,6 +119,21 @@ void NavigationHandler::humanDetectionCB(const std_msgs::Float64::ConstPtr& msg)
 {
   boost::unique_lock<boost::mutex> scoped_lock(mtx_);
   human_detection_score_ = msg->data;
+}
+
+
+bool NavigationHandler::enableBestEffortGoalCB(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
+{
+  p_enable_best_effort_goal_ = req.data;
+  ROS_INFO("[%s] %s navigation with best effort goal", name_.c_str(), p_enable_best_effort_goal_ ? "Enabled" : "Disabled");
+  res.success = true;
+  return true;
+}
+
+
+void NavigationHandler::robotPoseCB(const geometry_msgs::Pose::ConstPtr& msg)
+{
+  robot_pose_ = *msg;
 }
 
 
@@ -157,7 +189,7 @@ void NavigationHandler::startWithDetection(move_base_msgs::MoveBaseGoal goal)
 }
 
 
-void NavigationHandler::navigationLoop(move_base_msgs::MoveBaseGoal goal)
+void NavigationHandler::navigationLoop(const move_base_msgs::MoveBaseGoal& goal)
 {
   bool navigating = true;
   bool succeeded = false;
@@ -219,56 +251,180 @@ void NavigationHandler::navigationLoop(move_base_msgs::MoveBaseGoal goal)
 }
 
 
+void NavigationHandler::navigationAttemptGoal(const move_base_msgs::MoveBaseGoal& goal_msg)
+{
+  if (!enable_human_detection_) {
+    nav_ac_ptr_->sendGoal(goal_msg);
+    navigationLoop(goal_msg);
+  }
+  else {
+    startWithDetection(goal_msg);
+    navigationLoop(goal_msg);
+  }
+}
+
+
+void NavigationHandler::navigationDirect(const geometry_msgs::Pose& goal_pose)
+{
+  // get plan from move_base at current robot pose
+  nav_msgs::GetPlan srv{};
+  srv.request.start.pose = robot_pose_;   // current pose
+  srv.request.start.header.frame_id = "map";
+  srv.request.goal.pose = goal_pose;   // main goal
+  srv.request.goal.header.frame_id = "map";
+  if (!make_plan_client_.call(srv)) {
+    ROS_ERROR("[%s] Service call to make_plan failed", name_.c_str());
+    setTaskResult(false);
+    return;
+  }
+  // direct goal exists
+  if (srv.response.plan.poses.size() > 0) {
+    ROS_INFO("[%s] Starting navigation to goal", name_.c_str());
+    move_base_msgs::MoveBaseGoal goal_msg;
+    goal_msg.target_pose.pose = goal_pose;   // main goal
+    goal_msg.target_pose.header.frame_id = "map";
+    // start navigation
+    navigationAttemptGoal(goal_msg);
+    return;
+  }
+  // no direct goal
+  else {
+    ROS_INFO("[%s] No direct plan to goal exists", name_.c_str());
+    setTaskResult(false);
+    return;
+  }
+} 
+  
+
+void NavigationHandler::navigationBestEffort(const geometry_msgs::Pose& goal_pose)
+{
+  // try until main goal is reached or no more best effort goals can be found
+  while (true)
+  {
+    // choose between direct goal or best effort goal
+    nav_msgs::GetPlan srv{};
+    srv.request.start.pose = robot_pose_;   // current pose
+    srv.request.start.header.frame_id = "map";
+    srv.request.goal.pose = goal_pose;   // main goal
+    srv.request.goal.header.frame_id = "map"; 
+
+    ROS_WARN_STREAM("robot_pose: \n" 
+      << robot_pose_.position.x << '\n'
+      << robot_pose_.position.y << '\n'
+      << robot_pose_.position.z << '\n'
+      << robot_pose_.orientation.x << '\n'
+      << robot_pose_.orientation.y << '\n'
+      << robot_pose_.orientation.z << '\n'
+      << robot_pose_.orientation.w << '\n'
+    );
+
+    // srv.request.start.pose.position.x = -3.22;
+    // srv.request.start.pose.position.y = -0.962;
+    // srv.request.start.pose.position.z = 0.00644;
+    // srv.request.start.pose.orientation.w = 1.0;
+    // srv.request.start.header.frame_id = "map";
+    // srv.request.goal.pose.position.x = -1.02;
+    // srv.request.goal.pose.position.y = 1.09;
+    // srv.request.goal.pose.position.z = 0.00247;
+    // srv.request.goal.pose.orientation.w = 1.0;
+    // srv.request.goal.header.frame_id = "map"; 
+
+    // trying direct goal
+    // get plan from move_base at current robot pose
+    if (!make_plan_client_.call(srv)) {
+      ROS_ERROR("[%s] Service call to make_plan failed", name_.c_str());
+      setTaskResult(false);
+      return;
+    }
+    // direct goal exists, no best effort needed
+    if (srv.response.plan.poses.size() > 0) {
+      ROS_INFO("[%s] Starting navigation to goal", name_.c_str());
+      move_base_msgs::MoveBaseGoal goal_msg;
+      goal_msg.target_pose.pose = goal_pose;   // main goal
+      goal_msg.target_pose.header.frame_id = "map";
+      // start navigation
+      navigationAttemptGoal(goal_msg);
+      return;
+    }
+    // trying best effort goal
+    // get plan from planner_utils at current robot pose
+    if (!make_reachable_plan_client_.call(srv)) {
+      ROS_ERROR("[%s] Service call to make_reachable_plan failed", name_.c_str());
+      setTaskResult(false);
+      return;
+    }
+    // best effort goal exists
+    if (srv.response.plan.poses.size() > 0) {
+      ROS_INFO("[%s] Starting navigation to best effort goal", name_.c_str());
+      move_base_msgs::MoveBaseGoal goal_msg;
+      goal_msg.target_pose.pose = srv.response.plan.poses.back().pose;   // last pose in best effort path 
+      goal_msg.target_pose.header.frame_id = "map";
+      // start navigation
+      navigationAttemptGoal(goal_msg);
+      continue;   // continue while loop and try main goal again
+    }
+    // no best effort goal, failure
+    else {
+      ROS_INFO("[%s] No best effort plan to goal exists", name_.c_str());
+      setTaskResult(false);
+      return;
+    }
+  }
+}
+
+
 ReturnCode NavigationHandler::runTask(movel_seirios_msgs::Task& task, std::string& error_message)
 {
   task_active_ = true;
   task_parsed_ = false;
   start_ = ros::Time::now();
 
-  if (start_ActionClient())
-  {
+  if (start_ActionClient()) {
     ROS_INFO("[%s] Task payload %s", name_.c_str(), task.payload.c_str());
     json payload = json::parse(task.payload);
-    if (payload.find("position") != payload.end())
-    {
+    if (payload.find("position") != payload.end()) {
+      // input goal
       geometry_msgs::Pose goal_pose;
-
       goal_pose.position.x = payload["position"]["x"].get<float>();
       goal_pose.position.y = payload["position"]["y"].get<float>();
       goal_pose.position.z = payload["position"]["z"].get<float>();
-
       goal_pose.orientation.x = payload["orientation"]["x"].get<float>();
       goal_pose.orientation.y = payload["orientation"]["y"].get<float>();
       goal_pose.orientation.z = payload["orientation"]["z"].get<float>();
       goal_pose.orientation.w = payload["orientation"]["w"].get<float>();
-
-      move_base_msgs::MoveBaseGoal goal_msg;
-      goal_msg.target_pose.pose = goal_pose;
-      goal_msg.target_pose.header.frame_id = "map";
-
-      if (!enable_human_detection_)
-      {
-        nav_ac_ptr_->sendGoal(goal_msg);
-        //nav_ac_ptr_->waitForResult();
-        //setTaskResult(true);
-        navigationLoop(goal_msg);
+      // navigation
+      // best effort
+      if (p_enable_best_effort_goal_) {
+        ROS_INFO("[%s] Starting navigation - best effort: enabled", name_.c_str());
+        if (make_reachable_plan_client_.exists()) {
+          navigationBestEffort(goal_pose);
+        }
+        else {   // planner_utils service unavailable
+          std::string service_name = make_reachable_plan_client_.getService();
+          if (p_normal_nav_if_best_effort_unavailable_) {
+            ROS_WARN("[%s] Service %s is not available, using normal navigation", name_.c_str(), service_name.c_str());
+            navigationDirect(goal_pose);
+          }
+          else {
+            ROS_ERROR("[%s] Service %s is not available", name_.c_str(), service_name.c_str());
+            setTaskResult(false);
+          }
+        }
       }
-      else
-      {
-        startWithDetection(goal_msg);
-        navigationLoop(goal_msg);
+      // normal navigation
+      else {
+        ROS_INFO("[%s] Starting navigation - best effort: disabled", name_.c_str());
+        navigationDirect(goal_pose);
       }
     }
-    else
-    {
+    else {
       setMessage("Malformed payload Example: {\"position\":{\"x\":-4,\"y\":0.58,\"z\":0}, "
                  "\"orientation\":{\"x\":0,\"y\":0,\"z\":0.71,\"w\":0.69}}");
       error_message = message_;
       setTaskResult(false);
     }
   }
-  else
-  {
+  else {
     setMessage("Unable to talk to Move Base action server on " + p_navigation_server_);
     error_message = message_;
     setTaskResult(false);
@@ -299,4 +455,5 @@ void NavigationHandler::cancelTask()
   task_active_ = false;
   task_paused_ = false;
 }
+
 }  // namespace task_supervisor
