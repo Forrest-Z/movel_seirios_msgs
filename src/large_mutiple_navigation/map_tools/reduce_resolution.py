a36da50f9be0f3de984f89b90eaa9c28dd774e92
Navaneethgs <navaneetha@movel.ai>
2021-05-17 14:06:28 +0530

    large navigation1

---
+++ src/large_mutiple_navigation/map_tools/reduce_resolution.py
@@ -0,0 +1,21 @@
+import cv2
+ 
+src = cv2.imread('full.pgm', cv2.IMREAD_UNCHANGED)
+
+#percent by which the image is resized
+scale_percent = 50
+
+#calculate the 50 percent of original dimensions
+width = int(src.shape[1] * scale_percent / 100)
+height = int(src.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+
+# resize image
+output = cv2.resize(src, dsize)
+
+cv2.imwrite('full1.pgm',output) 
+# cv2.waitKey(0)
+# close all open windows
+# cv2.destroyAllWindows()
