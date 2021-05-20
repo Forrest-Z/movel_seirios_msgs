f216db3b06666076778027e5beaea23ca1970910
iris18 <iris@movel.ai>
2021-05-20 13:02:26 +0800

    lidar person tracking

---
+++ src/hdl_people_tracking/apps/hdl_people_tracking_nodelet.cpp
@@ -0,0 +1,240 @@
+#include <mutex>
+#include <memory>
+#include <iostream>
+#include <boost/format.hpp>
+
+#include <ros/ros.h>
+#include <pcl_ros/point_cloud.h>
+#include <tf_conversions/tf_eigen.h>
+#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/Twist.h>
+#include <pcl/filters/filter.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include "hdl_people_tracking/ClearTarget.h"
+#include <visualization_msgs/MarkerArray.h>
+#include <hdl_people_tracking/TrackArray.h>
+#include <hdl_people_tracking/ClusterArray.h>
+#include "hdl_people_tracking/Reset.h"
+#include <kkl/cvk/cvutils.hpp>
+#include <hdl_people_tracking/people_tracker.hpp>
+#include "std_msgs/Int32.h"
+
+namespace hdl_people_tracking {
+
+class HdlPeopleTrackingNodelet : public nodelet::Nodelet {
+public:
+  using PointT = pcl::PointXYZI;
+
+  HdlPeopleTrackingNodelet() {}
+  virtual ~HdlPeopleTrackingNodelet() {}
+
+  void onInit() override {
+    nh_ = getNodeHandle();
+    mt_nh = getMTNodeHandle();
+    private_nh = getPrivateNodeHandle();
+
+    tracker_.reset(new PeopleTracker(private_nh));
+    color_palette = cvk::create_color_palette(16);
+
+    tracks_pub_ = private_nh.advertise<hdl_people_tracking::TrackArray>("tracks", 10);
+    marker_pub_ = private_nh.advertise<visualization_msgs::MarkerArray>("markers", 10);
+    clusters_sub_ = nh_.subscribe("/hdl_people_detection_nodelet/clusters", 1, &HdlPeopleTrackingNodelet::callback, this);
+    target_sub_ = nh_.subscribe("/velocity_commands/target_id", 1, &HdlPeopleTrackingNodelet::cb, this);
+    cmd_vel_sub_ = nh_.subscribe("/cmd_vel", 1, &HdlPeopleTrackingNodelet::cmdVelCb, this);
+    //Service
+    reset_ = nh_.advertiseService("/hdl_people_tracking/reset", &HdlPeopleTrackingNodelet::reset, this);
+    reset_client_ = nh_.serviceClient<hdl_people_tracking::ClearTarget>("/hdl_people_tracking/clear_target", true);
+  }
+
+private:
+  void cb (const std_msgs::Int32 &msg){
+      target_id_ = int(msg.data);
+  }
+  
+  void cmdVelCb (const geometry_msgs::Twist &msg){
+      linear_x_ = msg.linear.x;
+      angular_z_ = msg.angular.z;
+  }
+
+  void callback(const hdl_people_tracking::ClusterArrayPtr& clusters_msg) {
+    // remove non-human detections
+    auto remove_loc = std::remove_if(clusters_msg->clusters.begin(), clusters_msg->clusters.end(), [=](const Cluster& cluster) { return !cluster.is_human; });
+    clusters_msg->clusters.erase(remove_loc, clusters_msg->clusters.end());
+
+    // update people tracker
+    tracker_->predict(clusters_msg->header.stamp, linear_x_, angular_z_);
+    tracker_->correct(clusters_msg->header.stamp, clusters_msg->clusters, target_id_);
+
+    // publish tracks msg
+    //if(tracks_pub.getNumSubscribers()) {
+      tracks_pub_.publish(create_tracks_msg(clusters_msg->header));
+    //}
+
+    // publish rviz markers
+    //if(marker_pub.getNumSubscribers()) {
+      marker_pub_.publish(create_tracked_people_marker(clusters_msg->header));
+    //}
+  }
+  
+  bool reset(hdl_people_tracking::Reset::Request &req, hdl_people_tracking::Reset::Response &res){
+    if (req.restart){
+      tracker_.reset(new PeopleTracker(private_nh));
+      hdl_people_tracking::ClearTarget srv;
+      srv.request.clear_target = true;
+      try {
+       reset_client_.call(srv);
+       res.restarted = true;
+       return true;
+      } 
+      catch (...)
+     {
+      ROS_ERROR("Failed to call service /hdl_people_tracking/clear_target");
+      res.restarted = false;
+      return false;
+     }
+      
+    }
+    else{
+      res.restarted = false;
+      return false;
+    }
+  }
+
+
+  hdl_people_tracking::TrackArrayConstPtr create_tracks_msg(const std_msgs::Header& header) const {
+    hdl_people_tracking::TrackArrayPtr tracks_msg(new hdl_people_tracking::TrackArray());
+    tracks_msg->header = header;
+    tracks_msg->tracks.resize(tracker_->people.size());
+    for(int i=0; i<tracker_->people.size(); i++) {
+      const auto& track = tracker_->people[i];
+      auto& track_msg = tracks_msg->tracks[i];
+
+      track_msg.id = track->id();
+      track_msg.age = (track->age(header.stamp)).toSec();
+      track_msg.pos.x = track->observed_position().x();
+      track_msg.pos.y = track->observed_position().y();
+      track_msg.pos.z = track->observed_position().z();
+      track_msg.vel.x = track->velocity().x();
+      track_msg.vel.y = track->velocity().y();
+      track_msg.vel.z = track->velocity().z();
+
+      Eigen::Matrix3d pos_cov = track->positionCov();
+      for(int k=0; k<3; k++) {
+        for(int j=0; j<3; j++) {
+          track_msg.pos_cov[k*3 + j] = pos_cov(k, j);
+        }
+      }
+
+      Eigen::Matrix3d vel_cov = track->velocityCov();
+      for(int k=0; k<3; k++) {
+        for(int j=0; j<3; j++) {
+          track_msg.vel_cov[k*3 + j] = vel_cov(k, j);
+        }
+      }
+
+      const Cluster* associated = boost::any_cast<Cluster>(&track->lastAssociated());
+      if(!associated) {
+        continue;
+      }
+
+      track_msg.associated.resize(1);
+      track_msg.associated[0] = (*associated);
+    }
+
+    return tracks_msg;
+  }
+
+  visualization_msgs::MarkerArrayConstPtr create_tracked_people_marker(const std_msgs::Header& header) const {
+    visualization_msgs::MarkerArrayPtr markers_ptr(new visualization_msgs::MarkerArray());
+
+    visualization_msgs::MarkerArray& markers = *markers_ptr;
+    markers.markers.reserve(tracker_->people.size() + 1);
+    markers.markers.resize(1);
+
+    visualization_msgs::Marker& boxes = markers.markers[0];
+    boxes.header = header;
+    boxes.action = visualization_msgs::Marker::ADD;
+    boxes.lifetime = ros::Duration(1.0);
+
+    boxes.ns = "boxes";
+    boxes.type = visualization_msgs::Marker::CUBE_LIST;
+    boxes.colors.reserve(tracker_->people.size());
+    boxes.points.reserve(tracker_->people.size());
+
+    boxes.pose.position.z = 0.0f;
+    boxes.pose.orientation.w = 1.0f;
+
+    boxes.scale.x = 0.5;
+    boxes.scale.y = 0.5;
+    boxes.scale.z = 1.2;
+
+    for(int i=0; i<tracker_->people.size(); i++) {
+      const auto& person = tracker_->people[i];
+      const auto& color = color_palette[person->id() % color_palette.size()];
+
+      if(person->correctionCount() < 5) {
+        continue;
+      }
+
+      std_msgs::ColorRGBA rgba;
+      rgba.r = color[2] / 255.0;
+      rgba.g = color[1] / 255.0;
+      rgba.b = color[0] / 255.0;
+      rgba.a = 0.6f;
+      markers.markers[0].colors.push_back(rgba);
+
+      geometry_msgs::Point point;
+      point.x = person->position().x();
+      point.y = person->position().y();
+      point.z = person->position().z();
+      markers.markers[0].points.push_back(point);
+
+      visualization_msgs::Marker text;
+      text.header = markers.markers[0].header;
+      text.action = visualization_msgs::Marker::ADD;
+      text.lifetime = ros::Duration(1.0);
+
+      text.ns = (boost::format("text%d") % person->id()).str();
+      text.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+      text.scale.z = 0.5;
+
+      text.pose.position = point;
+      text.pose.position.z += 0.7;
+      text.color.r = text.color.b = text.color.a = 1.0;
+      text.color.g = 250.0;
+      text.text = (boost::format("id:%d") % person->id()).str();
+
+      markers.markers.push_back(text);
+    }
+
+    return markers_ptr;
+  }
+
+private:
+  // ROS
+  ros::NodeHandle nh_;
+  ros::NodeHandle mt_nh;
+  ros::NodeHandle private_nh;
+
+  ros::Publisher tracks_pub_;
+  ros::Publisher marker_pub_;
+  ros::Subscriber clusters_sub_;
+  ros::Subscriber target_sub_;
+  ros::Subscriber cmd_vel_sub_;
+  boost::circular_buffer<cv::Scalar> color_palette;
+  float linear_x_, angular_z_;
+  std::unique_ptr<PeopleTracker> tracker_;
+  int target_id_;
+  ros::ServiceServer reset_;
+  ros::ServiceClient reset_client_; //to reset target and cancel goal
+};
+
+}
+
+
+PLUGINLIB_EXPORT_CLASS(hdl_people_tracking::HdlPeopleTrackingNodelet, nodelet::Nodelet)
---
+++ src/hdl_people_tracking/src/velocity_commands.cpp
@@ -0,0 +1,215 @@
+#include <hdl_people_tracking/people_tracker.hpp>
+#include <hdl_people_tracking/velocity_commands.hpp>
+
+VelocityCommands::VelocityCommands(): target_id_(-2), n_private("~"), following_(false), listener_(buffer_)
+{
+    if (!getParams() || !setupTopics())
+        {ROS_ERROR("Parameter error. Please try again.");}
+    ROS_INFO("Finished Initialization");
+    while(ros::ok()){
+    ros::spinOnce();
+    }
+
+}
+
+bool VelocityCommands::getParams()
+{   
+    ros::param::param<float>("~goal_dist_thresh", goal_dist_thresh_, 0.5);
+    ros::param::param<float>("~target_offset", target_offset_, 0.9);
+    ros::param::param<float>("~radius", radius_, 1.50);
+    ros::param::param<int>("~y_samples", y_samples_, 5);
+    return true;
+}
+
+bool VelocityCommands::setupTopics()
+{
+  //Publishers
+  cancel_pub_ = n_.advertise<actionlib_msgs::GoalID>("/move_base/cancel", 1);
+  goal_pub_ = n_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+  target_pub_ = n_.advertise<std_msgs::Int32>("/velocity_commands/target_id", 1);
+  //Subscribers
+  sub_ = n_.subscribe("/hdl_people_tracking_nodelet/tracks", 1, &VelocityCommands::callback, this);
+  feedback_sub_ = n_.subscribe("/move_base/feedback", 1, &VelocityCommands::feedback_cb, this);
+  pose_sub_ = n_.subscribe("/pose", 1, &VelocityCommands::pose_cb, this);
+  //Services
+  choose_target_ = n_.advertiseService("/hdl_people_tracking/choose_target", &VelocityCommands::choose_target, this);
+  clear_target_ = n_.advertiseService("/hdl_people_tracking/clear_target", &VelocityCommands::clear_target, this);
+  plan_client_ = n_.serviceClient<nav_msgs::GetPlan>("/move_base/GlobalPlanner/make_plan", true);
+
+  return true;
+}
+
+void VelocityCommands::pose_cb(const geometry_msgs::Pose &msg){
+    pose_ = msg;
+    return;
+}
+
+float VelocityCommands::calcDist(float x, float y){
+    float ans =  pow(x,2) + pow(y,2);
+    return sqrt(ans);
+}
+
+void VelocityCommands::callback(const hdl_people_tracking::TrackArray &msg)
+{   
+    //identities_[50] = {-1};
+    unordered_map_.clear();
+    unordered_map_.reserve(msg.tracks.size());
+	  for (hdl_people_tracking::Track track: msg.tracks)
+	  {   //identities_[idx] = track.id; idx++;
+                float depth = calcDist(track.pos.x, track.pos.y); 
+                std::pair<int,float> a_pair (track.id,depth);
+                unordered_map_.insert(a_pair);
+		if (track.id == target_id_ && depth > target_offset_){
+                nav_robot(track.pos.x, track.pos.y);
+                return;
+	        }
+    else {continue;}
+    }
+}
+
+void VelocityCommands::nav_robot(float position_x,float position_y){
+   // move_base client
+    geometry_msgs::PoseStamped laser_pose;
+    laser_pose.header.frame_id = "laser";
+    laser_pose.pose.position.x = position_x - 1.0;
+    laser_pose.pose.position.y = position_y;
+    laser_pose.pose.orientation.w = 1.0;
+    geometry_msgs::PoseStamped map_pose = buffer_.transform(laser_pose, "map");
+    float diff_x = map_pose.pose.position.x - goal_.pose.position.x;
+    float diff_y = map_pose.pose.position.y - goal_.pose.position.y;
+    float dist = calcDist(diff_x, diff_y);
+    if (dist >= goal_dist_thresh_ || following_ == false){
+    following_ = true;
+    nav_msgs::GetPlan srv;
+    populateClient(srv, map_pose.pose); 
+    goal_.header.frame_id = "map";
+    goal_.header.stamp = ros::Time::now();
+    try {
+    plan_client_.call(srv);
+    if (srv.response.plan.poses.size() > 0){
+    goal_= map_pose;
+    }
+    else{getNearestPseudoGoal(map_pose.pose);}
+    ROS_INFO_STREAM("SENDING GOAL's coordinates, x: " << goal_.pose.position.x << ", y: " << goal_.pose.position.y);
+    goal_pub_.publish(goal_);
+    return;
+    } 
+    catch (...)
+    {
+      ROS_ERROR("Failed to call service /move_base/GlobalPlanner/make_plan");
+    }
+    return;
+    }
+   }
+
+bool VelocityCommands::choose_target(hdl_people_tracking::ChooseTarget::Request &req, hdl_people_tracking::ChooseTarget::Response &res){
+    //bool exists = std::find(std::begin(identities_), std::end(identities_), req.target_id) != std::end(identities_);
+    bool cannot_find = unordered_map_.find(req.target_id) == unordered_map_.end();
+    if (!cannot_find){
+    if (unordered_map_.at(req.target_id) <= radius_)
+    {std_msgs::Int32 target_msg; 
+    target_id_ = req.target_id; 
+    res.target_selected_success = true; 
+    target_msg.data = target_id_; 
+    target_pub_.publish(target_msg);
+    return true; 
+    }
+    else {
+      ROS_ERROR("Target is outside radius."); 
+      res.target_selected_success = false; 
+      return false;}
+    }
+    if (cannot_find){
+    ROS_ERROR("Target not found."); 
+    res.target_selected_success = false; 
+    return false;
+    }
+  }
+
+bool VelocityCommands::clear_target(hdl_people_tracking::ClearTarget::Request &req, hdl_people_tracking::ClearTarget::Response &res){
+  if (req.clear_target) {
+    target_id_ = -2; 
+    res.target_cleared_success = true; 
+    actionlib_msgs::GoalID cancel_path;
+    cancel_pub_.publish(move_base_goal_id_);
+    cancel_pub_.publish(cancel_path);
+    return true;}
+  else {
+    res.target_cleared_success = false; 
+    return false;}
+}
+void VelocityCommands::getNearestPseudoGoal(geometry_msgs::Pose pos) {
+  for (int i = 1; i <= y_samples_; i++)
+  { float alpha_step = 0.3*i;
+    for (int j = 0; j < 4; j++)
+  {geometry_msgs::Pose estimated_nearby = pos;
+   if (j == 0)
+   {   estimated_nearby.position.x = pos.position.x + alpha_step*pos.position.x;
+       estimated_nearby.position.y = pos.position.y + alpha_step*pos.position.y;}
+   else if (j == 1)
+   {   estimated_nearby.position.x = pos.position.x + alpha_step*pos.position.x;
+       estimated_nearby.position.y = pos.position.y - alpha_step*pos.position.y;}
+   else if (j == 2)
+   {   estimated_nearby.position.x = pos.position.x - alpha_step*pos.position.x;
+       estimated_nearby.position.y = pos.position.y + alpha_step*pos.position.y;}
+   else if (j == 3)
+   {   estimated_nearby.position.x = pos.position.x - alpha_step*pos.position.x;
+       estimated_nearby.position.y = pos.position.y - alpha_step*pos.position.y;}
+    
+   nav_msgs::GetPlan srv;
+   populateClient(srv, estimated_nearby);
+   try {
+      plan_client_.call(srv);
+      ROS_INFO("iter %d, %d, plan size %lu", i, j, srv.response.plan.poses.size());
+      //! Check if plan to waypoint is viable
+      if (srv.response.plan.poses.size() > 0){
+          goal_.pose = estimated_nearby;return;}
+      }
+   catch (...)
+    {
+      ROS_ERROR("Failed to call service /move_base/GlobalPlanner/make_plan");
+    }
+  }
+  } 
+  ROS_ERROR("Failed to find a viable goal. Please check for obstacles.");} 
+
+
+
+
+void VelocityCommands::populateClient(nav_msgs::GetPlan &srv,
+                                      geometry_msgs::Pose target_pose) {
+  srv.request.start.header.frame_id = "map";
+  
+  srv.request.start.pose.position.x = pose_.position.x;
+  srv.request.start.pose.position.y = pose_.position.y;
+  srv.request.start.pose.position.z = pose_.position.z;
+  srv.request.start.pose.orientation.x = pose_.orientation.x;
+  srv.request.start.pose.orientation.y = pose_.orientation.y;
+  srv.request.start.pose.orientation.z = pose_.orientation.z;
+  srv.request.start.pose.orientation.w = pose_.orientation.w;
+
+  srv.request.goal.header.frame_id = "map";
+  srv.request.goal.pose.position.x = target_pose.position.x;
+  srv.request.goal.pose.position.y = target_pose.position.y;
+  srv.request.goal.pose.position.z = target_pose.position.z;
+  srv.request.goal.pose.orientation.x = target_pose.orientation.x;
+  srv.request.goal.pose.orientation.y = target_pose.orientation.y;
+  srv.request.goal.pose.orientation.z = target_pose.orientation.z;
+  srv.request.goal.pose.orientation.w = target_pose.orientation.w;
+
+  srv.request.tolerance = 0;
+}
+
+void VelocityCommands::feedback_cb(const move_base_msgs::MoveBaseActionFeedback::ConstPtr &msg) 
+{
+  move_base_goal_id_ = msg->status.goal_id;
+  return;
+}
+
+// Main function for the program
+int main(int argc, char** argv) {
+   ros::init(argc, argv, "velocity_commands_node");
+   VelocityCommands vc;
+ 
+   return 0;
+}
