9aff1a186090cb69aab8e8a51248537735892aa4
Robertsen Putra Sugianto <robertsen@movel.ai>
2021-06-16 13:59:14 +0700

    update map_editor to handle multi array of polygons

--- src/map_editor/src/map_editor.cpp
+++ src/map_editor/src/map_editor.cpp
@@ -6,40 +6,45 @@ bool MapEditor::updateCb(movel_seirios_msgs::DrawPolygon::Request &req,
     filename_ = req.filename;
     try
     {
-        std::vector<cv::Point> poly_points_;
         std::string nav_path = nav_map_path_ + filename_ + ".pgm";
         cv::Mat image = cv::imread(nav_path, cv::IMREAD_GRAYSCALE );
-        for (movel_seirios_msgs::Pixel& pixel : req.pixels)
-        {
-            cv::Point pt;
-            pt.x = pixel.x;
-            pt.y = pixel.y;
-            poly_points_.push_back(pt);
-        }
 
-        if (poly_points_.size() > 2)
+        for (movel_seirios_msgs::Polygon& polygon : req.polygons)
         {
-            cv::polylines(image, poly_points_, true, 0, line_width_, 8);
-            std::vector<std::vector<cv::Point>> contours;
-            contours.push_back(poly_points_);
-            cv::fillPoly(image, contours, 0, 8);
+            std::vector<cv::Point> poly_points_;
+            for (movel_seirios_msgs::Pixel& pixel : polygon.pixels)
+            {
+                cv::Point pt;
+                pt.x = pixel.x;
+                pt.y = pixel.y;
+                poly_points_.push_back(pt);
+            }
+
+            if (poly_points_.size() > 2)
+            {
+                cv::polylines(image, poly_points_, true, 0, line_width_, 8);
+                std::vector<std::vector<cv::Point>> contours;
+                contours.push_back(poly_points_);
+                cv::fillPoly(image, contours, 0, 8);
+            }
+            else if (poly_points_.size() == 2)
+            {
+                cv::line(image, poly_points_[0], poly_points_[1], 0, line_width_, 8);
+            }
+            else if (poly_points_.size() < 2)
+            {
+                ROS_INFO("Cannot process the number of points below 2!");
+                res.success = false;
+                return false;
+            }
+            cv::Mat image_gray = image;
+            cv::imwrite(nav_path, image_gray);
         }
-        else if (poly_points_.size() == 2)
-        {
-            cv::line(image, poly_points_[0], poly_points_[1], 0, line_width_, 8);
-        }
-        else if (poly_points_.size() < 2)
-        {
-            ROS_INFO("Cannot process the number of points below 2!");
-            res.success = false;
-            return false;
-        }
-        cv::Mat image_gray = image;
-        cv::imwrite(nav_path, image_gray);
 
         std_srvs::Trigger relaunch;
         if(!relaunch_map_server_.call(relaunch))
             ROS_INFO("Failed to call relaunch service for localization handler");
+            
     }
     catch (cv::Exception& e)
     {   
@@ -68,7 +73,6 @@ bool MapEditor::restoreMapCb(movel_seirios_msgs::DrawPolygon::Request &req,
     res.success = true;
     return true;
 }                                
-
 
 void MapEditor::loadParams()
 {
