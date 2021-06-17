261e0ecc448d1bace135c83472530ecd7c859b92
Ryan Arya Pratama <ryan.arya@movel.ai>
2021-06-17 18:33:28 +0800

    map_splitter; WIP, map splitting, compute transitions

--- src/map_splitter/include/map_splitter/map_splitter.h
+++ src/map_splitter/include/map_splitter/map_splitter.h
@@ -1,11 +1,15 @@
 #ifndef map_splitter_h
 #define map_splitter_h
 
-#include <ros/ros.h>
+#include <fstream>
+#include <iostream>
 #include <movel_seirios_msgs/StringTrigger.h>
 #include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include <ros/ros.h>
+#include <sys/stat.h>
 #include <yaml-cpp/yaml.h>
 
 class MapSplitter
@@ -26,10 +30,15 @@ private:
   int width_;
   int height_;
   int overlap_;
+  int scale_;
 
   // service callbacks
   bool splitMapSrvCb(movel_seirios_msgs::StringTrigger::Request &req,
                      movel_seirios_msgs::StringTrigger::Response &res);
+
+  void getMapBounds(cv::Mat &bigmap, int &top, int &left, int &bottom, int &right);
+
+  void scaleMap(cv::Mat &bigmap, cv::Mat &smallmap, int scale);
 };
 
 #endif
