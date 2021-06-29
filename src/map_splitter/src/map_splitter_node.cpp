a68f7fa4cde8675df98beb3506bd0029d990be3b
Ryan Arya Pratama <ryan.arya@movel.ai>
2021-06-29 14:36:33 +0800

    map_splitter/swapper; add install, licensing

--- src/map_splitter/src/map_splitter_node.cpp
+++ src/map_splitter/src/map_splitter_node.cpp
@@ -1,8 +1,14 @@
 #include <ros/ros.h>
 #include "map_splitter/map_splitter.h"
+#include <movel_hasp_vendor/license.h>
 
 int main(int argc, char** argv)
 {
+#ifdef MOVEL_LICENSE                                                                                                    
+  MovelLicense ml(34);                                                                                                   
+  if (!ml.login())                                                                                                      
+    return 1;                                                                                                           
+#endif
   ros::init(argc, argv, "map_splitter");
 
   MapSplitter ms;
