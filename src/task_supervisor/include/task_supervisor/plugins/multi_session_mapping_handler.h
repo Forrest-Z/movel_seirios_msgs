6a2f828760305d30bc41a5c93d280ab114c59c32
raine <raine@movel.ai>
2022-01-21 08:28:34 +0000

    fix costmap issue for dynamic mapping

--- src/task_supervisor/include/task_supervisor/plugins/multi_session_mapping_handler.h
+++ src/task_supervisor/include/task_supervisor/plugins/multi_session_mapping_handler.h
@@ -11,10 +11,7 @@ namespace task_supervisor
 class MultiSessionMappingHandler : public TaskHandler
 {
 private:
-  /**
-   * @brief Callback method to start move_base_dyn.launch
-   */
-  bool onStartMovebaseDyn(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res);
+
 
   /**
    * @brief Callback method to start mapping
@@ -64,11 +61,10 @@ private:
   std::vector<std::string> parseArgs(std::string payload);
 
   bool onStopCall(movel_seirios_msgs::StringTrigger::Request& req, movel_seirios_msgs::StringTrigger::Response& res);
-  bool onStopMovebaseDyn(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res);
 
   // Internal vars
   std::string path_;
-  unsigned int mapping_launch_id_ = 0;
+  //unsigned int mapping_launch_id_ = 0;
   unsigned int map_expander_launch_id_ = 0;
   unsigned int dyn_movebase_launch_id_ = 0;
   bool mapping_started_ = false;
