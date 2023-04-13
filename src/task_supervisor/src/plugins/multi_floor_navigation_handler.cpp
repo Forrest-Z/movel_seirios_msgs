3631422f2486fbcbd6be32f126366c14341b85d2
Billy Nugraha Sutandi <billy.nugraha.s@gmail.com>
2023-04-13 11:40:47 +0700

    fixed issue of MFN can't navigate to/from the map that has double doors

--- src/task_supervisor/src/plugins/multi_floor_navigation_handler.cpp
+++ src/task_supervisor/src/plugins/multi_floor_navigation_handler.cpp
@@ -407,10 +407,22 @@ bool MultiFloorNavigationHandler::pathGenerationHandle(std::string from_room, st
     std::string to_print_path = " ";
     std::vector<std::vector<float>> rcvd_coords {{},{}};
     
+    //Detection condition, so the src or dest will not be overwrite to new value
+    //Otherwise the MFN can't navigate to/from the map that has double doors
+    bool from_room_detected = false;
+    bool to_room_detected = false;
     for(int i = 0; i<map_nodes_.size();i++)
     {
-        if(map_nodes_[i]==from_room.c_str())src = i;
-        if(map_nodes_[i]==to_room.c_str())dest = i;
+        if(!from_room_detected && map_nodes_[i]==from_room.c_str())
+        {
+          src = i;
+          from_room_detected = true;
+        }
+        if(!to_room_detected && map_nodes_[i]==to_room.c_str())
+        {
+          dest = i;
+          to_room_detected = true;
+        }
     }
     if(src==999 || dest==999)
     {
