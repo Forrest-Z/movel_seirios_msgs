0ffcb9eb2a8a282d41615cf3ac7e4f9df49bcdcc
Billy Nugraha Sutandi <billy@movel.ai>
2023-10-20 10:20:59 +0700

    added MFN get map id service

--- src/multi_floor_nav_utils/include/multi_floor_nav_utils/mfn_save_utils.hpp
+++ src/multi_floor_nav_utils/include/multi_floor_nav_utils/mfn_save_utils.hpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <filesystem>
 #include "movel_seirios_msgs/MFNSaveTransitPoint.h"
 #include "movel_seirios_msgs/MFNSaveMapNames.h"
+#include "movel_seirios_msgs/MFNGetMapId.h"
 
 using namespace std;
+namespace fs = std::filesystem;
 
 class MFNSaveUtils{
     private:
@@ -22,6 +25,7 @@ class MFNSaveUtils{
     ros::NodeHandle n_;
     ros::ServiceServer save_node_names_service_;
     ros::ServiceServer save_transit_points_service_;
+    ros::ServiceServer get_map_id_service_; 
 
     MFNSaveUtils(ros::NodeHandle* nodehandle);
 
@@ -31,4 +35,6 @@ class MFNSaveUtils{
     void initializeServers();
     bool MFNSaveTransitPointsHandle(movel_seirios_msgs::MFNSaveTransitPoint::Request& ,movel_seirios_msgs::MFNSaveTransitPoint::Response& );
     bool MFNSaveNodeNamesHandle(movel_seirios_msgs::MFNSaveMapNames::Request& ,movel_seirios_msgs::MFNSaveMapNames::Response& );
+    string getMapIdFromTransitPoint(string& transit_point_file_path, string& map_id, geometry_msgs::Pose& transit_point_pose);
+    bool getMapIdHandle(movel_seirios_msgs::MFNGetMapId::Request& req, movel_seirios_msgs::MFNGetMapId::Response& res);
 };
